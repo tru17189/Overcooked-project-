a8cc0ba76878a693367c22b7ad0d24c8a3e53df0
tru17189 <32879914+tru17189@users.noreply.github.com>
2019-09-09 11:27:51 -0600

    6

---
+++ Assets/NewTable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewTable : MonoBehaviour
+{
+    public GameObject ingredient;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    public movableObject GetIngredient()
+    {
+        GameObject NewObject = Instantiate(ingredient);
+        movableObject movable = NewObject.GetComponent<movableObject>();
+        return movable;
+    }
+}
