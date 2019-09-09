f85cd7a2e8a30fce19805a67cda74954805ab28f
tru17189 <32879914+tru17189@users.noreply.github.com>
2019-09-09 11:22:08 -0600

    g

---
+++ Assets/barra.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+
+public class barra : MonoBehaviour
+{
+    Image TimeBar;
+    public float MaxTime = 5f;
+    float Timeleft;
+    float FlagTime;
+    public GameObject timesUpText;
+    private foodState state;
+    barra objeto;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timesUpText.SetActive(false);
+        TimeBar = GetComponent<Image>();
+        Timeleft = MaxTime;
+        state = GetComponent<foodState>();
+      
+    }
+
+    // Update is called once per frame
+     void Update()
+    {
+        //if (objeto.se)
+       // {
+         //   FlagTime = 1;
+        //}
+
+        //if (FlagTime == 1)
+        //{
+            if (Timeleft > 0)
+            {
+                Timeleft -= Time.deltaTime;
+                TimeBar.fillAmount = Timeleft / MaxTime;
+            }
+            else
+            {
+                timesUpText.SetActive(true);
+                MaxTime = 5f;
+                //Time.timeScale = 0;
+            }
+
+        //}
+        }
+        
+ 
+}
---
+++ Assets/cerca.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cerca : MonoBehaviour
+{
+  List<table> tablelist;
+  table current; 
+
+    //Start is called before the first frame update
+    void Start()
+    {
+        tablelist = new List<table>();
+    }
+
+    public table Getselected()
+    {
+        return current; 
+    }  
+
+    private void OnTriggerEnter(Collider other)
+    {
+        table f = other.gameObject.GetComponent<table>();
+        if (f != null) {
+            tablelist.Add(f);
+            //Debug.Log("Se agrego: " + f.name);
+        }
+        selectTable();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        table f = other.gameObject.GetComponent<table>();
+        if (f != null)
+        {
+            tablelist.Remove(f);
+            chopper fChopper = f.GetComponent<chopper>();
+            if (fChopper != null) {
+                fChopper.StartChooping();
+            }
+            //Debug.Log("Se elimino: " + f.name);
+        }
+        selectTable();
+    }
+
+    private void selectTable() {
+        if (current != null) {
+            current.RemoveHighlight(); 
+        }
+
+        if (tablelist.Count > 0)
+        {
+            table selected = null;
+            float Idistance = 1000f;
+            foreach (table f in tablelist)
+            {
+                float distance = Vector3.Distance(transform.position, f.transform.position);
+                if (distance < Idistance)
+                {
+                    Idistance = distance;
+                    selected = f;
+                }
+            }
+
+            if (selected != null)
+            {
+                //Debug.Log("El mas cercano es: " + selected.name);
+                selected.Highlight();
+                current = selected;
+                //transform.localScale += new Vector3(0.1F, 0, 0);
+            }
+        }
+    }
+
+    void Update()
+    {
+        selectTable(); 
+    }
+}
